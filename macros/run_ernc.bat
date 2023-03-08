1d93940a1b846fdb98891380bf840fd1c91e3c44
George Wenzel <georgewenzel7@gmail.com>
2023-03-08 17:22:17 -0300

    added comments to batch file

--- macros/run_ernc.bat
+++ macros/run_ernc.bat
@@ -1,4 +1,6 @@
 call %USERPROFILE%\Anaconda3\Scripts\activate
 call conda activate plp
+: Change directory to code location
 cd %USERPROFILE%\MacrosPLP_py
+: Run ernc script
 call python macros/ernc.py
