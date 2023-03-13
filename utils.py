a19bec8bc5f56dfab73e3202568efec355a9cb19
George Wenzel <georgewenzel7@gmail.com>
2023-03-13 17:59:01 -0300

    return Path instead of open handle

--- utils.py
+++ utils.py
@@ -107,4 +107,4 @@ def is_valid_file(parser, arg):
     if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
-        return open(arg, 'r')  # return an open file handle
+        return Path(arg)
