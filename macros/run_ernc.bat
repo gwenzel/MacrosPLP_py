99f71843e47e2063d9410bed10a3a9cfa4ea03b4
George Wenzel <georgewenzel7@gmail.com>
2023-03-14 12:49:03 -0300

    updated batch file

--- macros/run_ernc.bat
+++ macros/run_ernc.bat
@@ -4,3 +4,15 @@ call conda activate plp
 cd %USERPROFILE%\MacrosPLP_py
 : Run ernc script
 call python macros/ernc.py
+: Sleep 5 seconds to show results or errors
+timeout 5
+
+: On server:
+: set file=%1
+: call %USERPROFILE%\Anaconda3\Scripts\activate
+: call conda activate plp
+: :cd D:\PLP_macros_python
+: :call python macros/ernc.py
+: call ernc -f %file%
+: : Sleep 5 seconds to show results or errors
+: timeout 5
