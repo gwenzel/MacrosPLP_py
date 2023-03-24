759a936519e1df0fd78c4dc928d12cc3c00dc23c
George Wenzel <georgewenzel7@gmail.com>
2023-03-24 13:05:26 -0300

    [dda] calculated consumption and got rid of mappings

--- macros/demanda.py
+++ macros/demanda.py
@@ -45,53 +45,28 @@ HORA_DICT = {'H%s' % i: i for i in range(1, 25)}
 def dda_por_barra_to_row_format(iplp_path, write_to_csv=False):
     df = pd.read_excel(iplp_path, sheet_name="DdaPorBarra")
     #print(df)
-    keys = ["#", "Coordinado", "Cliente", "Perfil día tipo",
-            "Verificador consumo", "Barra Consumo", "Factor Barra Consumo"]
+    keys = ["Coordinado", "Cliente", "Profile",
+            "Barra Consumo", "Factor Barra Consumo"]
     df_as_dict = df.to_dict()
     #print(df_as_dict["Coordinado"][0] is np.nan)
     new_dict = {key:{} for key in keys}
     idx = 0
-    for i in range(598):
-        for barra in range(1, 23+1): 
-            #print(df_as_dict["Barra Consumo %s" % barra][i] is not np.nan)
+    #df.set_index(['#','Coordinado','Cliente','Perfil día tipo','Verificador consumo']).stack()
+    for i in range(len(df)):
+        for barra in range(1, 23+1):
             if df_as_dict["Barra Consumo %s" % barra][i] is not np.nan:
-                new_dict["#"][idx] = i
                 new_dict["Coordinado"][idx] = df_as_dict["Coordinado"][i]
                 new_dict["Cliente"][idx] = df_as_dict["Cliente"][i]
-                new_dict["Perfil día tipo"][idx] = df_as_dict["Perfil día tipo"][i]
-                new_dict["Verificador consumo"][idx] = df_as_dict["Verificador consumo"][i]
+                new_dict["Profile"][idx] = df_as_dict["Perfil día tipo"][i]
                 new_dict["Barra Consumo"][idx] = df_as_dict["Barra Consumo %s" % barra][i]
                 new_dict["Factor Barra Consumo"][idx] = df_as_dict["Factor Barra Consumo %s" % barra][i]
             idx+=1
     #print(new_dict)
     df_dda_por_barra = pd.DataFrame(new_dict).reset_index(drop=True)
+    df_dda_por_barra['Coordinado'] = df_dda_por_barra['Coordinado'].fillna("NA")
     if write_to_csv:
         df_dda_por_barra.to_csv(iplp_path.parent / 'DdaPorBarra_rows.csv')
     return df_dda_por_barra
-
-
-def get_mappings(df_dda_por_barra):
-    df_dda_por_barra['Coordinado'] = df_dda_por_barra['Coordinado'].fillna("NA")
-    
-    # Get Coord-Cliente to Profile mapping
-    df_dda_por_barra['Coord-Cliente'] = df_dda_por_barra.apply(
-        lambda x: (x['Coordinado'], x['Cliente']), axis=1)
-    map_cc_to_profile = df_dda_por_barra[['Coord-Cliente', 'Perfil día tipo']]
-    map_cc_to_profile = map_cc_to_profile.set_index('Coord-Cliente').to_dict()
-    
-    # Get Coord-Cliente-Barra to Factor Consumo mapping
-    df_dda_por_barra['Coord-Cliente-Barra'] = df_dda_por_barra.apply(
-        lambda x: (x['Coordinado'], x['Cliente'], x['Barra Consumo']), axis=1)
-    map_ccb_to_cf = df_dda_por_barra[['Coord-Cliente-Barra', 'Factor Barra Consumo']]
-    map_ccb_to_cf = map_ccb_to_cf.set_index('Coord-Cliente-Barra').to_dict()
-
-    # Get list of Barras for each Coord-Cliente
-    map_cc_to_barras = {}
-    for cc in df_dda_por_barra['Coord-Cliente'].unique():
-        map_cc_to_barras[cc] = df_dda_por_barra[
-            df_dda_por_barra['Coord-Cliente']==cc]['Barra Consumo'].to_list()
-    
-    return map_cc_to_profile, map_ccb_to_cf, map_cc_to_barras
 
 
 def get_monthly_demand(iplp_path):
@@ -100,6 +75,10 @@ def get_monthly_demand(iplp_path):
     }
     df = pd.read_excel(iplp_path, sheet_name='DdaEnergia',
                        converters=date_converter)
+    
+    # Drop rows if column #, Coordinado or Cliente is nan
+    df = df.dropna(subset=['#', 'Coordinado', 'Cliente'], how='any')
+
     # Clean data
     cols_to_drop = ['#', 'Coordinado.1', 'Cliente.1', 'Perfil',
                     'Clasificacion SEN', 'Clasificacion ENGIE']
@@ -130,10 +109,44 @@ def get_hourly_profiles(iplp_path):
     return df
 
 
-def get_all_profiles(map_cc_to_profile, map_ccb_to_cf, map_cc_to_barras,
-                     df_monthly_demand, df_hourly_profiles):
+def get_blockly_profiles(df_hourly_profiles, block2day):
+    df = df_hourly_profiles.rename(
+        columns={'Perfil día tipo': 'Profile',
+                 'Mes': 'Month',
+                 'Hora': 'Hour'})
+    df = pd.merge(df, block2day, on=['Month','Hour'], how='left')
+    df = df.drop('Hour', axis=1)
+    df = df.groupby(['Profile','Month','Block']).mean().reset_index()
+    return df
+
+
+def get_all_profiles(blo_eta, block2day,
+                     df_monthly_demand, df_hourly_profiles, df_dda_por_barra):
+
+    list_barras = df_dda_por_barra['Barra Consumo'].unique().tolist()
+
+    # Turn hourly profiles to profiles by block
+    df_blockly_profiles = get_blockly_profiles(df_hourly_profiles, block2day)
+
+
+    # Add data to monthly demand df
+    df_monthly_demand['Year'] = pd.to_datetime(df_monthly_demand['Date']).dt.year
+    df_monthly_demand['Month'] = pd.to_datetime(df_monthly_demand['Date']).dt.month
+    df_monthly_demand = df_monthly_demand.drop(['Date'], axis=1)
+
+    # Merge dataframes
+    df = pd.merge(df_monthly_demand, df_dda_por_barra, on=['Coordinado','Cliente'])
+    df = pd.merge(df, df_blockly_profiles, on=['Profile','Month'])
+
+    # Calculate consumption, group by Barra and sum
+    df['Consumo'] = df.apply(lambda x: x['Demand'] * x['Factor Barra Consumo'] * x['PowerFactor'], axis=1)
+    df = df.drop(['Demand', 'Factor Barra Consumo', 'PowerFactor'], axis=1)
+    df = df.groupby(['Year','Month','Block', 'Barra Consumo']).sum(numeric_only=True).reset_index()
+    df = df.sort_values(by=['Year','Month','Barra Consumo','Block'])
+
     import pdb; pdb.set_trace()
     pass
+    #return df_all_profiles
 
 
 @timeit
@@ -159,10 +172,6 @@ def main():
     df_dda_por_barra = dda_por_barra_to_row_format(
         iplp_path, write_to_csv=True)
 
-    # Get mappings
-    map_cc_to_profile, map_ccb_to_cf, map_cc_to_barras = \
-        get_mappings(df_dda_por_barra)
-
     # Get monthly demand from Sheet "DdaEnergia"
     logger.info('Processing DdaEnergia sheet')
     df_monthly_demand = get_monthly_demand(iplp_path)
@@ -174,8 +183,8 @@ def main():
     # Generate dataframe with profiles per Etapa
     logger.info('Generating dataframes with profiles per Etapa per Barra')
     df_all_profiles = get_all_profiles(
-        map_cc_to_profile, map_ccb_to_cf, map_cc_to_barras,
-        df_monthly_demand, df_hourly_profiles)
+        blo_eta, block2day,
+        df_monthly_demand, df_hourly_profiles, df_dda_por_barra)
 
 
     # Print to plpdem and uni_plpdem
