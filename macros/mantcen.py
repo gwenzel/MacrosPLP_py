3a447c382daf16db2df7c94ec1a724b1bad7ea0d
George Wenzel <georgewenzel7@gmail.com>
2023-04-12 14:41:52 -0400

    [mantcen] added comments

--- macros/mantcen.py
+++ macros/mantcen.py
@@ -10,6 +10,30 @@ logger = create_logger('mantcen')
 
 
 def main():
+    # LlenadoMantConvenc
+    # Fill Mantcen with remaining mantcen
+    # No cíclicos, cíclicos, restricciones de gas, genmin
+
+    # Plpmance
+    # Read Centrales and get PnomMax and PnomMin
+    # Filter out if X, and count if F
+
+    # Check sheet GxFalla to define tramos and Nfalla
+
+    # Initialize array with Pnom
+
+    # Check which units have maintenance, and validate their names
+
+    # Update pmin/pmax data
+
+    # translate to Etapas/Bloques
+
+    # count Etapas with maintenance
+
+    # write dat file
+
+    # Write report in sheet (?)
+
     pass
 
 if __name__ == "__main__":
