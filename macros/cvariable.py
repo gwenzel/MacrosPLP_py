c78444117138b76a9369b038f7ed2fe3bfcac573
George Wenzel <georgewenzel7@gmail.com>
2023-04-25 16:20:13 -0400

    [cvar] initial block

--- macros/cvariable.py
+++ macros/cvariable.py
@@ -1,7 +1,8 @@
 from utils import ( get_project_root,
                     get_iplp_input_path,
                     check_is_path,
-                    create_logger
+                    create_logger,
+                    process_etapas_blocks
 )
 import pandas as pd
 
@@ -10,7 +11,18 @@ logger = create_logger('cvariable')
 
 
 def main():
-    pass
+    # Get input file path
+    logger.info('Getting input file path')
+    iplp_path = get_iplp_input_path()
+    path_inputs = iplp_path.parent / "Temp"
+    check_is_path(path_inputs)
+    path_dat = iplp_path.parent / "Temp" / "Dat"
+    check_is_path(path_dat)
+
+    # Get Hour-Blocks-Etapas definition
+    logger.info('Processing block to etapas files')
+    blo_eta, _, _ = process_etapas_blocks(path_dat)
+    blo_eta = blo_eta.drop(['Tasa'], axis=1)
 
 if __name__ == "__main__":
     main()
