e73bcbcd0b2ceaeeba37a03564342b47eab478fe
George Wenzel <georgewenzel7@gmail.com>
2023-08-01 16:33:51 -0400

    added filter files script

--- macros/filter_files.py
+++ macros/filter_files.py
@@ -1,83 +1,110 @@
-'''
-Sub Filter_Files()
-    Application.DisplayAlerts = False
-    'Application.ScreenUpdating = False
-    On Error GoTo NoFolder:
-    
-    Dim d_folder As String, d_folder_Plexos As String, d_file As String, a As String
-    Dim i As Integer, j As Integer, n As Integer, m As Integer, count As Integer
-    
-    d_folder = Hoja01.Range("C1").value & "\Sal"
-    If Dir(d_folder, vbDirectory) = "" Then: MkDir d_folder
-    d_folder_Plexos = Hoja01.Range("C1").value & "\Dat Plexos"
-    If Dir(d_folder_Plexos, vbDirectory) = "" Then: MkDir d_folder_Plexos
-    d_folder = Hoja01.Range("C1").value & "\Dat"
-    If Dir(d_folder, vbDirectory) = "" Then: MkDir d_folder
-    
-    On Error GoTo NoFile:
-    
-    n = Sheet24.Cells(Rows.count, 1).End(xlUp).Row
-    m = Sheet24.Cells(1, Columns.count).End(xlToLeft).Column
-    d_file = d_folder & "\block2day.csv"
-    Open d_file For Output As #1
-    d_file = d_folder_Plexos & "\block2day.csv"
-    Open d_file For Output As #2
-        For i = 1 To n
-            For j = 1 To m
-                Write #1, Sheet24.Cells(i, j).value,
-                Write #2, Sheet24.Cells(i, j).value,
-            Next j
-            Write #1,
-            Write #2,
-        Next i
-    Close #1
-    Close #2
-    
-    d_file = d_folder & "\plpparam.csv"
-    n = Hoja07.Cells(Rows.count, 1).End(xlUp).Row
-    Open d_file For Output As #1
-        Write #1, "Nº Hidr:", Hoja20.Range("D4").value
-        Write #1, "Nº Sim:", Hoja20.Range("D2").value
-        Write #1, "Nº Bloq:", Hoja07.Range("G5").value
-        Write #1,
-        Write #1, "", "Mes", "Año"
-        Write #1, "Inicio:", Month(Hoja07.Range("B5").value), Year(Hoja07.Range("B5").value)
-        Write #1, "Fin:", Month(Hoja07.Cells(n, 4).value), Year(Hoja07.Cells(n, 4).value)
-    Close #1
-    
-    d_file = d_folder & "\plpetapas.csv"
-    n = Hoja07.Cells(Rows.count, 1).End(xlUp).Row
-    n_blo = Hoja01.Range("D5")
-    count = 1
-    Open d_file For Output As #1
-        Write #1, "Etapa", "Year", "Month", "Block"
-        For i = 5 To n
-            For j = 1 To n_blo
-                Write #1, count, Year(Hoja07.Cells(i, 2).value), Month(Hoja07.Cells(i, 2).value), j
-                count = count + 1
-            Next j
-        Next i
-    Close #1
-    
-    Call IdEtapa_Hidro
-    
-    Application.DisplayAlerts = True
-    'Application.ScreenUpdating = True
-    Exit Sub
-    
-NoFolder:
-    a = MsgBox("No se crearon las carpetas. Revise directorio de archivos", vbCritical, "Error al crear carpetas")
-    Application.DisplayAlerts = True
-    'Application.ScreenUpdating = True
-    Exit Sub
-NoFile:
-    a = MsgBox("No se crearon los archivos. Revise que no estén en uso", vbCritical, "Error al crear archivos")
-    Application.DisplayAlerts = True
-    'Application.ScreenUpdating = True
-    Exit Sub
-End Sub
+import pandas as pd
+
+from utils.utils import (define_arg_parser,
+                         get_iplp_input_path,
+                         check_is_path,
+                         timeit,
+                         create_logger
+)
+from openpyxl.utils.datetime import from_excel
+
+logger = create_logger('filter_files')
 
 
+@timeit
+def create_block2day(iplp_path, path_dat, sheet_name='Block2Day'):
+    # Read data from Excel file starting from cell A1 to M25
+    df = pd.read_excel(iplp_path, header=None, sheet_name=sheet_name,
+                       usecols="A:M", nrows=25)
+    # Save data to CSV file
+    csv_file = path_dat / "block2day.csv"
+    df.to_csv(csv_file, index=False, header=False)
 
 
-'''
+@timeit
+def create_plpparam_and_plpetapas(iplp_path, path_dat):
+    df_etapas = pd.read_excel(iplp_path, sheet_name='Etapas', skiprows=3)
+    df_hidro = pd.read_excel(iplp_path, sheet_name='Hidrología',
+                             skiprows=1, header=None, usecols='B:D', nrows=6)
+    
+    df_etapas['Inicial'] = df_etapas['Inicial'].apply(from_excel)
+    df_etapas['Final'] = df_etapas['Final'].apply(from_excel)
+    
+    etapas = df_etapas.shape[0]
+    bloques = df_etapas.loc[0, 'Nº Bloques']
+    n = etapas - 1
+
+    # Prepare the data to write to plpparam
+    data = [
+        ("Nº Hidr:", df_hidro.iloc[2,2]),
+        ("Nº Sim:", df_hidro.iloc[0,2]),
+        ("Nº Bloq:", bloques),
+        ("", "", ""),
+        ("", "Mes", "Año"),
+        ("Inicio:", f"{df_etapas.loc[0, 'Inicial'].month}", f"{df_etapas.loc[0, 'Inicial'].year}"),
+        ("Fin:", f"{df_etapas.loc[n, 'Final'].month}", f"{df_etapas.loc[n, 'Final'].year}")
+    ]
+    # Convert the data to a DataFrame
+    df = pd.DataFrame(data)
+
+    # Write the DataFrame to plpparam
+    csv_file = path_dat / "plpparam.csv"
+    df.to_csv(csv_file, index=False, header=None, encoding='latin1')
+
+    # Prepare the data to write to plpetapas
+    plp_etapas_data = [("Etapa", "Year", "Month", "Block")]
+    for e in range(etapas):
+        for b in range(bloques): 
+            plp_etapas_data.append(
+                ( 1 + (12*e+b), f"{df_etapas.loc[e, 'Inicial'].year}",
+                f"{df_etapas.loc[e, 'Inicial'].month}", b + 1)
+            )
+    
+    # Convert the data to a DataFrame
+    df = pd.DataFrame(plp_etapas_data)
+
+    # Write the DataFrame to plpetapas
+    csv_file = path_dat / "plpetapas.csv"
+    df.to_csv(csv_file, index=False, header=None, encoding='latin1')
+
+
+@timeit
+def main():
+    '''
+    Main routine
+    '''
+    # Get input file path
+    logger.info('Getting input file path')
+    parser = define_arg_parser()
+    iplp_path = get_iplp_input_path(parser)
+
+    logger.info('Checking if folders exist, create them otherwise')
+
+    path_inputs = iplp_path.parent / "Temp"
+    path_inputs.mkdir(parents=True, exist_ok=True)
+    check_is_path(path_inputs)
+
+    path_dat = iplp_path.parent / "Temp" / "Dat"
+    path_dat.mkdir(parents=True, exist_ok=True)
+    check_is_path(path_dat)
+
+    path_sal = iplp_path.parent / "Temp" / "Sal"
+    path_sal.mkdir(parents=True, exist_ok=True)
+    check_is_path(path_sal)
+
+    path_dat_plexos = iplp_path.parent / "Temp" / "Dat Plexos"
+    path_dat_plexos.mkdir(parents=True, exist_ok=True)
+    check_is_path(path_dat_plexos)
+
+    logger.info('Create block2day')
+    create_block2day(iplp_path, path_dat)
+    create_block2day(iplp_path, path_dat_plexos)
+
+    logger.info('Create plpparam and plpetapas')
+    create_plpparam_and_plpetapas(iplp_path, path_dat)
+
+    logger.info('Finished successfully')
+
+
+if __name__ == "__main__":
+    main()
