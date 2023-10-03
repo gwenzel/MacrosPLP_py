3f4bd680f93b01309d3abac4db335a04f543fcc4
George Wenzel <georgewenzel7@gmail.com>
2023-10-03 12:45:29 -0300

    [inflows] finalized shuffle

--- macros/inflows.py
+++ macros/inflows.py
@@ -5,11 +5,11 @@ Generate plpaflce.dat file with water inflows
 Generate Storage_NaturalInflow files for Plexos
 
 '''
-from calendar import monthrange
 from datetime import datetime
-from utils.utils import (define_arg_parser, get_daily_indexed_df,
+from utils.utils import (define_arg_parser,
                          get_iplp_input_path,
-                         check_is_path, process_etapas_blocks)
+                         check_is_path,
+                         process_etapas_blocks)
 from utils.logger import create_logger
 from openpyxl.utils.datetime import from_excel
 import pandas as pd
@@ -97,11 +97,11 @@ def get_df_daily(blo_eta, iplp_path):
     return df_daily
 
 
-def get_list_of_units(series_inflows):
-    return series_inflows.index.get_level_values('CENTRAL').unique().tolist()
+def get_list_of_units(df_all_inflows):
+    return df_all_inflows.index.get_level_values('CENTRAL').unique().tolist()
 
 
-def get_list_of_hid(df_all_inflows):
+def get_list_of_hyd(df_all_inflows):
     return df_all_inflows.index.get_level_values('INDHID').unique().tolist()
 
 
@@ -109,12 +109,13 @@ def get_df_all_inflows(iplp_path, blo_eta):
     series_inflows = read_inflow_data(iplp_path)
     df_daily = get_df_daily(blo_eta, iplp_path)
     # Join dataframes using outer to keep all data
-    new_index = ['YEAR', 'MONTH', 'DAY', 'WEEK_NAME', 'ETAPA']
+    new_index = ['YEAR', 'MONTH', 'DAY', 'WEEK_NAME']
     df_all_inflows = df_daily.set_index(new_index)
     df_all_inflows = df_all_inflows.join(series_inflows, how='outer')
     # Remove WEEK_NAME from index
-    df_all_inflows = df_all_inflows.reset_index().drop('WEEK_NAME', axis=1)
-    new_index = ['CENTRAL', 'YEAR', 'MONTH', 'DAY', 'ETAPA', 'INDHID']
+    df_all_inflows = df_all_inflows.reset_index().drop(
+        ['WEEK_NAME', 'ETAPA'], axis=1)
+    new_index = ['CENTRAL', 'YEAR', 'MONTH', 'DAY', 'INDHID']
     df_all_inflows = df_all_inflows.set_index(new_index)
     df_all_inflows = df_all_inflows.sort_index(
         level=['CENTRAL', 'YEAR', 'MONTH', 'DAY'])
@@ -122,7 +123,7 @@ def get_df_all_inflows(iplp_path, blo_eta):
 
 
 def print_plexos_inflows_all(df_all_inflows, path_pib):
-    list_of_hid = get_list_of_hid(df_all_inflows)
+    list_of_hyd = get_list_of_hyd(df_all_inflows)
     # format plexos, aux dataframe to unstack and print
     df_all_inflows['Inflows'] = df_all_inflows['Inflows'].apply(
         lambda x: round(x, 2))
@@ -130,17 +131,16 @@ def print_plexos_inflows_all(df_all_inflows, path_pib):
     df_aux = df_aux.reset_index()
     df_aux = df_aux.rename(
         columns={'CENTRAL': 'NAME'})
-    df_aux = df_aux.drop('ETAPA', axis=1)
     df_aux['PERIOD'] = 1
     df_aux = df_aux[
-        ['NAME', 'YEAR', 'MONTH', 'DAY', 'PERIOD'] + list_of_hid]
+        ['NAME', 'YEAR', 'MONTH', 'DAY', 'PERIOD'] + list_of_hyd]
     # print all data file
     df_aux.to_csv(path_pib / 'Storage_NaturalInflow.csv', index=False)
 
 
 def print_plexos_inflows_separate(df_all_inflows, path_pib):
-    list_of_hid = get_list_of_hid(df_all_inflows)
-    for indhid in list_of_hid:
+    list_of_hyd = get_list_of_hyd(df_all_inflows)
+    for indhid in list_of_hyd:
         mask = (df_all_inflows.index.get_level_values('INDHID') == indhid)
         inflows_aux = df_all_inflows[mask]
         # format
@@ -158,10 +158,57 @@ def print_plexos_inflows_separate(df_all_inflows, path_pib):
             path_pib / f'Storage_NaturalInflow_{indhid:02d}.csv', index=False)
 
 
-def shuffle_hidrologies(df_configsim, df_all_inflows):
-    import pdb; pdb.set_trace()
+def get_shuffled_hydrologies(blo_eta, iplp_path, df_configsim):
+    '''
+    Get shuffled hydrologies per day
 
-    return df_all_inflows
+    Data from configsim refers to Etapas, so we need to turn it into 
+    YEAR-MONTH-DAY data and get rid of Etapas
+    '''
+    df_configsim.index.names = ['ETAPA']
+    df_daily = get_df_daily(blo_eta, iplp_path).drop('WEEK_NAME', axis=1)
+    df_shuffled_hyd = df_daily.join(df_configsim, on='ETAPA')
+    df_shuffled_hyd = df_shuffled_hyd.drop('ETAPA', axis=1)
+    df_shuffled_hyd = df_shuffled_hyd.set_index(['YEAR', 'MONTH', 'DAY'])
+
+    df_shuffled_hyd = df_shuffled_hyd.stack()
+    df_shuffled_hyd.index.names = ['YEAR', 'MONTH', 'DAY', 'SHUFFLED_HYD']
+    df_shuffled_hyd.name = 'INDHID'
+    df_shuffled_hyd = df_shuffled_hyd.reset_index()
+    return df_shuffled_hyd
+
+
+def shuffle_hidrologies(blo_eta, iplp_path, df_configsim, df_all_inflows):
+    '''
+    Shuffle hydrologies according to ConfigSim
+    '''
+    # Get stacked dataframe with mapping from original hydrology (INDHID) to
+    # new shuffled hydrology (SHUFFLED_HYD) for each day
+    df_shuffled_hyd = get_shuffled_hydrologies(
+        blo_eta, iplp_path, df_configsim)
+
+    # Shuffling hydrologies
+    # For each unit, merge df_shuffled_hyd with inflows data coming from
+    # df_df_all_inflows
+    list_of_df = []
+    list_of_units = get_list_of_units(df_all_inflows)
+    for unit in list_of_units:
+        #  merge for each unit and build new dataframe
+        merge_cols = ['YEAR', 'MONTH', 'DAY', 'INDHID']
+        df_shuffled_aux = df_shuffled_hyd.merge(
+            df_all_inflows.loc[unit], how='left', on=merge_cols)
+        df_shuffled_aux['CENTRAL'] = unit
+        df_shuffled_aux = df_shuffled_aux.drop('INDHID', axis=1)
+        df_shuffled_aux = df_shuffled_aux.rename(
+            columns={'SHUFFLED_HYD': 'INDHID'})
+        list_of_df.append(df_shuffled_aux)
+
+    # Concatenate aux dataframes and reformat
+    df_final = pd.concat(list_of_df)
+    df_final = df_final.set_index(
+        ['CENTRAL', 'YEAR', 'MONTH', 'DAY', 'INDHID'])
+
+    return df_final
 
 
 def main():
@@ -191,14 +238,14 @@ def main():
 
     logger.info('Shuffling inflows according to ConfigSim')
     df_configsim = read_configsim(iplp_path)
-    df_all_inflows = shuffle_hidrologies(df_configsim, df_all_inflows)
+    df_all_inflows = shuffle_hidrologies(
+        blo_eta, iplp_path, df_configsim, df_all_inflows)
 
     logger.info('Printing inflows in plexos format')
     print_plexos_inflows_all(df_all_inflows, path_pib)
     print_plexos_inflows_separate(df_all_inflows, path_pib)
 
     logger.info('Process finished successfully')
-
 
 
 if __name__ == "__main__":
