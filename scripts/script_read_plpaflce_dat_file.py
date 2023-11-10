209638493dd080bcd7ca7c9e28547fa29d12c07a
George Wenzel <georgewenzel7@gmail.com>
2023-11-10 14:54:18 -0300

    [scripts] compare inflows

---
+++ scripts/script_read_plpaflce_dat_file.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+filename = r"C:\Users\BH5873\OneDrive - ENGIE\Bureau\plpaflce_py.dat"
+
+list_of_cen = []
+dict_cen_values = {}
+list_of_dfs = []
+
+with open(filename) as f:
+    lines = f.readlines()
+    for idx, line in enumerate(lines):
+        if "# Nombre de la central" in line:
+            current_cen = lines[idx + 1].strip().replace("'", "")
+            print(current_cen)
+            list_of_cen.append(current_cen)
+        if "# Mes   Bloque    Caudal" in line:
+            lines_with_values = int(lines[idx-1].split()[0])
+            text_cen = lines[idx + 1:idx + 1 + lines_with_values]
+            value_cen = [list(line.split()) for line in text_cen]
+            dict_cen_values[current_cen] = value_cen
+            columns = ['Mes', 'Bloque']
+            columns += ['H%s' % i for i in range(1,21)]
+            df = pd.DataFrame(
+                dict_cen_values[current_cen],
+                columns=columns)
+            df['Cen'] = current_cen
+            list_of_dfs.append(df)
+
+df_all = pd.concat(list_of_dfs)
+df_all['Mes'] = df_all['Mes'].astype(int)
+df_all['Bloque'] = df_all['Bloque'].astype(int)
+
+
+df_all.to_csv('afluentes_py.csv')
