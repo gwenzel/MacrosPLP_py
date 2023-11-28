71f9bcc7f393ec9e0f8e6111a3bea6ff895e350d
George Wenzel <georgewenzel7@gmail.com>
2023-11-28 11:50:38 -0300

    [tests] fixed typos

--- postpro/test/test_filtro_plp.py
+++ postpro/test/test_filtro_plp.py
@@ -16,11 +16,11 @@ class Test_Filtro_PLP_Windows(unittest.TestCase):
         # Check tasa first month
         mask1 = blo_eta['Year'] == 2023
         mask2 = blo_eta['Month'] == 1
-        tasa_val = blo_eta[mask1 & mask2]['Tasa'].mean()    
+        tasa_val = blo_eta[mask1 & mask2]['Tasa'].mean()
         self.assertEqual(tasa_val, 1.0)
 
         # Check tasa last month
         mask1 = blo_eta['Year'] == 2051
         mask2 = blo_eta['Month'] == 3
-        tasa_val = blo_eta[mask1 & mask2]['Tasa'].mean()    
+        tasa_val = blo_eta[mask1 & mask2]['Tasa'].mean()
         self.assertEqual(round(tasa_val, 6), 14.651901)
