859d9ae7e50729aff43f69eaef3ef722b9d319f2
George Wenzel <georgewenzel7@gmail.com>
2023-12-26 10:58:52 -0300

    [manli manlix] print dfs con index

--- macros/manlix.py
+++ macros/manlix.py
@@ -137,11 +137,11 @@ def get_manlix_changes(df_capmax_manlix_ab: pd.DataFrame,
     list_of_dfs = []
 
     if print_values:
-        df_capmax_manlix_ab.to_csv(path_df / 'df_manlix_ab.csv', index=False)
-        df_capmax_manlix_ba.to_csv(path_df / 'df_manlix_ba.csv', index=False)
-        df_v.to_csv(path_df / 'df_manlix_v.csv', index=False)
-        df_r.to_csv(path_df / 'df_manlix_r.csv', index=False)
-        df_x.to_csv(path_df / 'df_manlix_x.csv', index=False)
+        df_capmax_manlix_ab.to_csv(path_df / 'df_manlix_ab.csv')
+        df_capmax_manlix_ba.to_csv(path_df / 'df_manlix_ba.csv')
+        df_v.to_csv(path_df / 'df_manlix_v.csv')
+        df_r.to_csv(path_df / 'df_manlix_r.csv')
+        df_x.to_csv(path_df / 'df_manlix_x.csv')
 
     for line in manlix_lines:
         # Get diff vector to detect changes
