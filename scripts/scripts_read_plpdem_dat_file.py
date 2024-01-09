c204ea77ea42fe2cb2e92e6953ce1847af8cd03d
George Wenzel <georgewenzel7@gmail.com>
2024-01-09 10:51:33 -0300

    [scripts] add plpdem

---
+++ scripts/scripts_read_plpdem_dat_file.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+filename = r"C:\Users\BH5873\ENGIE\Equipo Estudios - Biblioteca Equipo Estudios\08. Modelos\202311 - MejorasPLP\comparaciones\dem\plpdem_nc_v24.dat"
+
+list_of_items = []
+dict_item2value = {}
+list_of_dfs = []
+
+with open(filename) as f:
+    file_lines = f.readlines()
+    for idx, line in enumerate(file_lines):
+        if '# Nombre de la Barra\n' in line:
+            current_item = file_lines[idx + 1].strip().replace("'", "")
+            print(current_item)
+            list_of_items.append(current_item)
+        if '# Mes  Etapa   Demanda\n' in line:
+            n_items = int(file_lines[idx-1].split()[0])
+            text_item = file_lines[idx + 1:idx + 1 + n_items]
+            value_item = [list(line.split()) for line in text_item]
+            dict_item2value[current_item] = value_item
+            df = pd.DataFrame(
+                dict_item2value[current_item],
+                columns=['Mes', 'Etapa', 'Demanda'])
+            df['Dem'] = current_item
+            list_of_dfs.append(df)
+df_all = pd.concat(list_of_dfs)
+df_all['Mes'] = df_all['Mes'].astype(int)
+df_all['Etapa'] = df_all['Etapa'].astype(int)
+df_all['Demanda'] = df_all['Demanda'].astype(float)
+
+
+df_all_pivot_cvar = df_all.pivot(
+    index='Etapa', columns='Dem', values=['Demanda'])
+
+df_all_pivot_cvar.to_csv(r"C:\Users\BH5873\ENGIE\Equipo Estudios - Biblioteca Equipo Estudios\08. Modelos\202311 - MejorasPLP\comparaciones\dem\dem_nc_v24.csv")
