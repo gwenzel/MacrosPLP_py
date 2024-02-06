ea91fd35b14a0ef7a1e6f9eaf8f7df19cb0aab42
George Wenzel <georgewenzel7@gmail.com>
2024-02-06 09:45:06 -0300

    [bug] fixed logger name

--- macros/mat.py
+++ macros/mat.py
@@ -64,7 +64,7 @@ def main():
     # Add destination folder to logger
     path_log = iplp_path.parent / "Temp" / "log"
     check_is_path(path_log)
-    add_file_handler(logger, 'plpblo', path_log)
+    add_file_handler(logger, 'plpmat', path_log)
 
     # Number of iterations
     num_iter = read_num_iter(iplp_path)
