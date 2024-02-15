43c05ecfda1963ee38bc2f13f9675b7a0f2928eb
George Wenzel <georgewenzel7@gmail.com>
2024-02-15 19:30:21 -0300

    [bar] error management

--- macros/bar.py
+++ macros/bar.py
@@ -66,6 +66,7 @@ def print_plpbar_full(path_inputs: Path, df_barras: pd.DataFrame):
     df_aux = df_aux.fillna(0)  # convert empty values (Trf) to 0
     df_aux.index = df_aux.index + 1
     df_aux = df_aux.reset_index()
+    df_aux['Tension'] = df_aux['Tension'].astype(int)
     df_aux['FlagDDA'] = df_aux['FlagDDA'].astype(int)
     df_aux['FlagGx'] = df_aux['FlagGx'].astype(int)
     df_aux = df_aux[['index', 'BARRA', 'Tension', 'FlagDDA', 'FlagGx']]
@@ -78,6 +79,7 @@ def print_plpbar_full(path_inputs: Path, df_barras: pd.DataFrame):
     lines += ['     %s' % len(df_barras)]
     lines += ['# Numero       Nombre                                 '
               '      Tension   FL FI']
+    import pdb; pdb.set_trace()
     lines += [df_aux.to_string(
         index=False, header=False, formatters=formatter_plpbar_full)]
     write_lines_from_scratch(lines, path_plpbar_full)
@@ -86,10 +88,10 @@ def print_plpbar_full(path_inputs: Path, df_barras: pd.DataFrame):
 def get_barras_info(iplp_path: Path, add_flag_falla: bool = False):
     if not add_flag_falla:
         return pd.read_excel(iplp_path, sheet_name="Barras",
-                             skiprows=4, usecols="A:C,E:F")
+                             skiprows=4, usecols="A:C,E:F").dropna(how='any')
     else:
         return pd.read_excel(iplp_path, sheet_name="Barras",
-                             skiprows=4, usecols="A:C,E:G")
+                             skiprows=4, usecols="A:C,E:G").dropna(how='any')
 
 
 @timeit
@@ -97,37 +99,45 @@ def main():
     '''
     Main routine
     '''
-    # Get input file path
-    logger.info('Getting input file path')
-    parser = define_arg_parser()
-    iplp_path = get_iplp_input_path(parser)
-    path_inputs = iplp_path.parent / "Temp"
-    check_is_path(path_inputs)
-    path_dat = iplp_path.parent / "Temp" / "Dat"
-    check_is_path(path_dat)
+    try:
+        # Get input file path
+        logger.info('Getting input file path')
+        parser = define_arg_parser()
+        iplp_path = get_iplp_input_path(parser)
+        path_inputs = iplp_path.parent / "Temp"
+        check_is_path(path_inputs)
+        path_dat = iplp_path.parent / "Temp" / "Dat"
+        check_is_path(path_dat)
 
-    # Add destination folder to logger
-    path_log = iplp_path.parent / "Temp" / "log"
-    check_is_path(path_log)
-    add_file_handler(logger, 'barras', path_log)
+        # Add destination folder to logger
+        path_log = iplp_path.parent / "Temp" / "log"
+        check_is_path(path_log)
+        add_file_handler(logger, 'barras', path_log)
 
-    # Get if uninodal or not
-    # UNINODAL = False
+        # Get if uninodal or not
+        # UNINODAL = False
 
-    # If uninodal, print dat file directly
-    logger.info('Printing uni_plpbar')
-    print_uni_plpbar(path_inputs)
+        # If uninodal, print dat file directly
+        logger.info('Printing uni_plpbar')
+        print_uni_plpbar(path_inputs)
 
-    # Else, get list of all barras
-    logger.info('Get list of all barras')
-    df_barras = get_barras_info(iplp_path)
+        # Else, get list of all barras
+        logger.info('Get list of all barras')
+        df_barras = get_barras_info(iplp_path)
 
-    # Print to file
-    logger.info('Printing plpbar')
-    print_plpbar(path_inputs, df_barras)
+        import pdb; pdb.set_trace()
 
-    logger.info('Printing plpbar_full')
-    print_plpbar_full(path_inputs, df_barras)
+        # Print to file
+        logger.info('Printing plpbar')
+        print_plpbar(path_inputs, df_barras)
+
+        logger.info('Printing plpbar_full')
+        print_plpbar_full(path_inputs, df_barras)
+
+        logger.info('Process finished successfully')
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        logger.error('Process finished with errors. Check above for details')
 
 
 if __name__ == "__main__":
