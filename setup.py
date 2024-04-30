fe5b4e8259f2ca64c377a9ea77cb8648e4d61d1b
George Wenzel <georgewenzel7@gmail.com>
2024-04-30 19:00:28 -0400

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name='macros_plp',
-    version='1.0.0',
+    version='1.1.0',
     description=('A package to replace PLP VBA macros'),
     author='George Wenzel',
     packages=find_packages(),
