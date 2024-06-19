1a1c2e3e195f24e68fc8054b1902ea9dc0dc89c1
George Wenzel <georgewenzel7@gmail.com>
2024-06-19 19:57:51 -0400

    [scripts] WIP curtailment with Pmax(t)

--- scripts/curtailment_v3.py
+++ scripts/curtailment_v3.py
@@ -28,7 +28,7 @@ from pathlib import Path
 
 pd.set_option('display.precision', 2)
 
-cur_dir = Path(r"C:\Users\BH5873\OneDrive - ENGIE\Bureau\BE Mar24 PLP")
+cur_dir = Path(r"C:\Users\BH5873\ENGIE\Equipo Estudios - Biblioteca Equipo Estudios\08. Modelos\202405 - Nuevo Curtailment\BE Mar24 PLP")
 # cur_dir = Path(os.getcwd())
 inputs_path = Path(cur_dir, "Output_Filter_Plexos")
 # Input Data files
@@ -42,9 +42,12 @@ INPUT_FILES = {
         "ener_file_name": "outEnerg_24H.csv"
     }
 }
+# Inputs from IPLP df
+rating_factor_file = Path(cur_dir, "df_ernc_rf_final.csv")
+df_centrales_file = Path(cur_dir, "df_centrales.csv")
 # Input Dict files
 zonas_file = Path(cur_dir, "BarsZones.csv")
-gen2zone_file = Path(cur_dir, "ID_Gen_PLP-Plexos.csv")
+tec2enable_file = Path(cur_dir, "enable_curtailment.csv")
 # Create output folder
 output_folder = Path(cur_dir, "Output_Curtailment")
 output_folder.mkdir(exist_ok=True)
@@ -88,7 +91,7 @@ def load_data(time_resolution):
     return df_cur, df_ener
 
 
-def load_dicts(zonas_file, gen2zone_file):
+def load_dicts(zonas_file, df_centrales_file, tec2enable_file):
     """
     Load the data required for analysis.
 
@@ -100,22 +103,107 @@ def load_dicts(zonas_file, gen2zone_file):
     df_zonas = pd.read_csv(zonas_file, encoding="latin1", low_memory=False)
     dict_node2zone = dict(zip(df_zonas["Node"], df_zonas["Zone"]))
 
-    # Define dict from Gen to Node in df_gen2zone
-    df_gen2zone = pd.read_csv(gen2zone_file, encoding="latin1",
-                              low_memory=False)
-    dict_gen2node = dict(zip(df_gen2zone["Gen_Name"],
-                             df_gen2zone["Bar"]))
-    dict_gen2pmax = dict(zip(df_gen2zone["Gen_Name"],
-                             df_gen2zone["PM x [MW]"]))
-    dict_gen2enable = dict(zip(df_gen2zone["Gen_Name"],
-                               df_gen2zone["Enable Curtailment"]))
+    # Read df_centrales. It has already filtered the generators
+    # with no participation in the dispatch
+    df_centrales = pd.read_csv(df_centrales_file, encoding="latin1",
+                               low_memory=False)
+
+    # Define dict from Gen to Node in df_centrales
+    dict_gen2node = dict(zip(df_centrales["Nombre"],
+                             df_centrales["Barra"]))
+    dict_gen2pmax = dict(zip(df_centrales["Nombre"],
+                             df_centrales["Pmax"]))
+
+    # Read tec2enable dictionary from file
+    df_tec2enable = pd.read_csv(tec2enable_file, encoding="latin1",
+                                low_memory=False)
+    dict_tec2enable = dict(zip(df_tec2enable.iloc[:, 0],
+                               df_tec2enable.iloc[:, 1]))
+    # Add column Enable Curtailment mapping with Fuel column
+    df_centrales["Enable Curtailment"] = df_centrales["Fuel"].map(
+        dict_tec2enable)
+    dict_gen2enable = dict(zip(df_centrales["Nombre"],
+                               df_centrales["Enable Curtailment"]))
     return dict_node2zone, dict_gen2node, dict_gen2pmax, dict_gen2enable
 
 
-# def load_gen2pmax_per_year(df_pmax_file):
+def get_df_rating_factors(final_year):
+    # Read rating factors and clean
+    df_rating_factor = pd.read_csv(rating_factor_file, encoding="latin1",
+                                   low_memory=False)
+    # Parse DateFrom as datetime
+    df_rating_factor["DateFrom"] = pd.to_datetime(df_rating_factor["DateFrom"])
+    # Filter by end year
+    df_rating_factor = df_rating_factor[df_rating_factor["Year"] <= final_year]
+    # Drop Day, DaysInMonth, Year-Month
+    df_rating_factor = df_rating_factor.drop(
+        ["Day", "DaysInMonth", "Year-Month"], axis=1)
+    initial_datefrom = df_rating_factor["DateFrom"].min()
+
+    # For each row, replace DateFrom, with ini DateFrom + Initial_Eta - 1
+    # Each Etapa represents a month
+    df_rating_factor["DateFrom"] = df_rating_factor.apply(
+        lambda x: initial_datefrom + pd.DateOffset(
+            months=x["Initial_Eta"] - 1), axis=1)
+    # Replace Year and Month with the ones from DateFrom
+    df_rating_factor["Year"] = df_rating_factor["DateFrom"].dt.year
+    df_rating_factor["Month"] = df_rating_factor["DateFrom"].dt.month
+    # Drop Initial_Eta
+    df_rating_factor = df_rating_factor.drop(["Initial_Eta", "DateFrom"], axis=1)
+    # Rename Name to Gen, Value [Mw] to Pmax
+    df_rating_factor = df_rating_factor.rename(
+        columns={"Name": "Gen", "Value [MW]": "Pmax"})
+    # Set Year, Month, Gen as index, and for duplicates, keep the last row
+    df_rating_factor = df_rating_factor.drop_duplicates(
+        subset=["Year", "Month", "Gen"], keep="last")
+    df_rating_factor = df_rating_factor.set_index(["Year", "Month", "Gen"])
+    return df_rating_factor
 
 
-def validate_inputs(df_cur, df_ener, dict_node2zone, dict_gen2node):
+def load_gen2pmax_per_month(df_ener, dict_gen2pmax, time_resolution="Block"):
+    # Get time limits
+    # initial_year = df_ener.index.get_level_values("Year").min()
+    # initial_month = df_ener.index.get_level_values("Month").min()
+    final_year = df_ener.index.get_level_values("Year").max()
+    # final_month = df_ener.index.get_level_values("Month").max()
+
+    # Get clean rating factors dataframe
+    df_rating_factor = get_df_rating_factors(final_year)
+
+    # Build dataframe, using time indexes from df_ener, and columns from
+    # dict_gen2pmax
+    df_pmax_per_month = pd.DataFrame(index=df_ener.index,
+                                     columns=dict_gen2pmax.keys())
+    # Fill with Pmax values
+    for gen, pmax in dict_gen2pmax.items():
+        df_pmax_per_month[gen] = pmax
+    # Pass to long format
+    df_pmax_per_month = df_pmax_per_month.stack().reset_index()
+    df_pmax_per_month.columns = ["Year", "Month", time_resolution,
+                                 "Gen", "Pmax"]
+    import pdb; pdb.set_trace()
+    # Overwrite Pmax with rating factors
+    df_pmax_per_month = pd.merge(df_pmax_per_month, df_rating_factor,
+                                 left_on=["Year", "Month", "Gen"],
+                                 right_index=True, how="left")
+    import pdb; pdb.set_trace()
+    # Go back to wide format
+    df_pmax_per_month = df_pmax_per_month.set_index(
+        ["Year", "Month", time_resolution, "Gen"])
+    df_pmax_per_month = df_pmax_per_month.unstack()
+    df_pmax_per_month.columns = df_pmax_per_month.columns.droplevel()
+    import pdb; pdb.set_trace()
+    # Detect when value changes, and fill with previous value
+
+    df_pmax_per_month = df_pmax_per_month.fillna(method="ffill")
+
+    import pdb; pdb.set_trace()
+
+    return df_pmax_per_month
+
+
+def validate_inputs(df_cur, df_ener, dict_node2zone, dict_gen2node,
+                    dict_gen2enable):
     # Validate that generators are aligned
     '''
     for gen, node in dict_gen2node.items():
@@ -127,15 +215,17 @@ def validate_inputs(df_cur, df_ener, dict_node2zone, dict_gen2node):
     print("Checking if all generators are in all files")
     print("Generators not found will not be considered for curtailment")
     for gen in df_ener.columns:
-        if gen not in dict_gen2node.keys():
-            print(f"Gen {gen} not found in Gen2Node file")
-        if gen not in df_cur.columns:
-            print(f"Gen {gen} not found in Curtailment file")
+        if gen in dict_gen2enable.keys():
+            if gen not in dict_gen2node.keys():
+                print(f"Gen {gen} not found in Gen2Node file")
+            if gen not in df_cur.columns:
+                print(f"Gen {gen} not found in Curtailment file")
     for gen in df_cur.columns:
-        if gen not in dict_gen2node.keys():
-            print(f"Gen {gen} not found in Gen2Node file")
-        if gen not in df_ener.columns:
-            print(f"Gen {gen} not found in Energy file")
+        if gen in dict_gen2enable.keys():
+            if gen not in dict_gen2node.keys():
+                print(f"Gen {gen} not found in Gen2Node file")
+            if gen not in df_ener.columns:
+                print(f"Gen {gen} not found in Energy file")
 
 
 def process_inputs(df_cur, df_ener, dict_node2zone, dict_gen2node,
@@ -162,18 +252,6 @@ def process_inputs(df_cur, df_ener, dict_node2zone, dict_gen2node,
     return df_all
 
 
-'''
-def group_data(df_all, time_resolution="Block"):
-    # Group values by zone
-    df_all_grouped = df_all.groupby(
-        ['Year', 'Month', time_resolution, 'Zone']).sum()
-    # Calculate grouped values per zone
-    df_all_grouped['Curtailment %'] = \
-        df_all_grouped['Curtailment'] / df_all_grouped['Energy+Curtailment']
-    return df_all_grouped
-'''
-
-
 def get_pmax(dict_gen2node, dict_node2zone, dict_gen2pmax,
              dict_gen2enable):
     # Create dataframe with each generador by zone and its Pmax percentage
@@ -186,6 +264,8 @@ def get_pmax(dict_gen2node, dict_node2zone, dict_gen2pmax,
     df_gen_data['Pmax'] = df_gen_data['Pmax'].fillna(0)
     df_gen_data['Pmax'] = df_gen_data['Pmax'] * df_gen_data[
         'Enable Curtailment']
+    # Fix Pmax value if Energy+Curtailment is > 0 and Enable Curtailment is 1
+    # TODO
     return df_gen_data
 
 
@@ -204,8 +284,8 @@ def redistribute_totals(df_all, df_gen_data,
     7. Pmax si participa es igual a Pmax si participa en la iteración 0
     8. Proceso iterativo:
         a. Calcular factor de prorrata
-        b. Calcular punto de operación sugerido
-        c. Calcular punto de operación saturando con la Energía Disponible
+        b. Calcular Punto de Operación sugerido
+        c. Calcular Punto de Operación saturando con la Energía Disponible
         d. Calcular la nueva energía disponible
         e. Calcular Energía Asignada, sumando Puntos Op. hasta el actual
         f. Calcular nueva colocación total
@@ -215,7 +295,7 @@ def redistribute_totals(df_all, df_gen_data,
         Participa si Energía Disponible > 0
     9. Calcular Punto de Operación final
     10. Calcular curtailment como la diferencia entre Energy+Curtailment y
-    el punto de operación final
+    el Punto de Operación final
     11. Formatear salidas
     '''
     # 1. Copiar el porcentaje de curtailment correspondiente a la zona
@@ -439,11 +519,13 @@ def main():
         print('--Loading data')
         df_cur, df_ener = load_data(time_resolution)
         dict_node2zone, dict_gen2node, dict_gen2pmax, dict_gen2enable = \
-            load_dicts(zonas_file, gen2zone_file)
+            load_dicts(zonas_file, df_centrales_file, tec2enable_file)
+        df_pmax_per_month = load_gen2pmax_per_month(df_ener, dict_gen2pmax)
 
         # Validate inputs
         print('--Validating inputs')
-        validate_inputs(df_cur, df_ener, dict_node2zone, dict_gen2node)
+        validate_inputs(df_cur, df_ener, dict_node2zone, dict_gen2node,
+                        dict_gen2enable)
 
         # Process data
         print('--Processing data')
