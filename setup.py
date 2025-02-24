bca3ffcf185cf921219765e018afb5c90ede6349
George Wenzel <20341243+gwenzel@users.noreply.github.com>
2025-02-24 17:40:02 -0300

    version 2.4.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name='macros_plp',
-    version='2.3.1',
+    version='2.4.0',
     description=('A package to replace PLP VBA macros'),
     author='George Wenzel',
     packages=find_packages(),
